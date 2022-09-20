fb9d6959a4f991ae0a83d2da181c4e3a2782adb1
bugkuska <62003765+bugkuska@users.noreply.github.com>
2022-09-20 15:19:39 +0700

    Create ir_code.ino

---
+++ esp32rainmaker/ir_code.ino
@@ -0,0 +1,28 @@
+/*************************************************************************************
+ *  Created By: Tauseef Ahmad
+ *  Created On: 28 July, 2022
+ *  
+ *  YouTube Video: https://youtu.be/inVhQaKLdlk
+ *  
+ *  *********************************************************************************
+ *  Install the following libraries
+ *  1. IRremote
+ ***********************************************************************************/
+
+#include <IRremote.h>
+const byte IR_RECEIVE_PIN = 14;
+
+void setup(){
+  Serial.begin(9600);
+  IrReceiver.begin(IR_RECEIVE_PIN); // Start the IR receiver
+}
+void loop(){
+  if (IrReceiver.decode()) {
+  String ir_code = String(IrReceiver.decodedIRData.command, HEX);
+  if(ir_code.equals("0")) {IrReceiver.resume();return; }
+  
+  Serial.println(ir_code);
+  IrReceiver.resume();
+  }
+  delay(1000);
+}
