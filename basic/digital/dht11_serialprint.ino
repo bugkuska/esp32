81d94424ca07283e4c6161f51d42e917603d757f
bugkuska <62003765+bugkuska@users.noreply.github.com>
2023-02-03 05:22:02 +0700

    Update dht11_serialprint.ino

--- basic/digital/dht11_serialprint.ino
+++ basic/digital/dht11_serialprint.ino
@@ -7,13 +7,13 @@
 
 #include "DHT.h"
 
-#define DHTPIN 16     // Digital pin connected to the DHT sensor
+#define DHTPIN 15     // Digital pin connected to the DHT sensor
 // Feather HUZZAH ESP8266 note: use pins 3, 4, 5, 12, 13 or 14 --
 // Pin 15 can work but DHT must be disconnected during program upload.
 
 // Uncomment whatever type you're using!
-#define DHTTYPE DHT11   // DHT 11
-//#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+//#define DHTTYPE DHT11   // DHT 11
+#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
 //#define DHTTYPE DHT21   // DHT 21 (AM2301)
 
 // Connect pin 1 (on the left) of the sensor to +5V
@@ -31,9 +31,7 @@ DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
   Serial.begin(9600);
-  Serial.println(F("DHTxx test!"));
-
-  dht.begin();
+  dht.begin();  
 }
 
 void loop() {
