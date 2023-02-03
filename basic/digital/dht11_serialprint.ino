2c1ee7768cba00b20a7e71007232acbe35c59124
bugkuska <62003765+bugkuska@users.noreply.github.com>
2023-02-03 07:25:43 +0700

    Update dht11_serialprint.ino

--- basic/digital/dht11_serialprint.ino
+++ basic/digital/dht11_serialprint.ino
@@ -7,7 +7,7 @@
 
 #include "DHT.h"
 
-#define DHTPIN 15     // Digital pin connected to the DHT sensor
+#define DHTPIN 18     // Digital pin connected to the DHT sensor
 // Feather HUZZAH ESP8266 note: use pins 3, 4, 5, 12, 13 or 14 --
 // Pin 15 can work but DHT must be disconnected during program upload.
 
