cb8d60b839c3fabd1fc03bd6a5ad500e8975b68a
bugkuska <62003765+bugkuska@users.noreply.github.com>
2023-06-05 15:03:35 +0700

    Update connect2wifi.ino

--- basic/connect2wifi.ino
+++ basic/connect2wifi.ino
@@ -2,21 +2,26 @@
 
 char ssid[] = ""; //ชื่อ SSID ที่เราต้องการเชื่อมต่อ        
 char pass[] = ""; //รหัสผ่าน WI-FI
- 
+ #define wifiLed 2  //กำหนดตัวแปร
 void setup()
 {
-   Serial.begin(9600);
+ 
+ pinMode(wifiLed, OUTPUT);  //กำหนดโหมด GPIO ให้เป็นแบบ OUTPUT
+ digitalWrite(wifiLed, LOW); //กำหนดค่าเริ่มต้นให้ Led built-in ดับ
+ 
+   Serial.begin(9600); 
    Serial.println("Starting...");
    WiFi.begin(ssid,pass);
    while (WiFi.status() != WL_CONNECTED)
    {
     delay(250);
-    Serial.print(".");
+    Serial.print(".");  //ถ้าเชื่อมต่อ Wi-Fi ไม่สำเร็จให้ Serial print .....................
    }
   Serial.println("WiFi Connected");
   Serial.println("IP Address: ");
   Serial.println(WiFi.localIP());
  
+ digitalWrite(wifiLed, HIGH);  //สั่งให้ Led built-in ติดหลังจากเชื่อมต่อ Wi-Fi ได้
  }
 
 void loop()
