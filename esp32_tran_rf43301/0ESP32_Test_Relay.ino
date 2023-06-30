06508429393ea692f810145129e60d779088c6cd
bugkuska <62003765+bugkuska@users.noreply.github.com>
2023-06-30 11:25:03 +0700

    Create 0ESP32_Test_Relay.ino

---
+++ esp32_tran_rf43301/0ESP32_Test_Relay.ino
@@ -0,0 +1,59 @@
+#define relay1        25
+#define relay2        26
+#define relay3        33 
+#define relay4        32
+//==========Setup==========//
+void setup()
+{
+  Serial.begin(9600);
+  delay(100);
+
+  pinMode(relay1,OUTPUT);
+  pinMode(relay2,OUTPUT);
+  pinMode(relay3,OUTPUT);
+  pinMode(relay4,OUTPUT);
+
+  digitalWrite(relay1,LOW);
+  digitalWrite(relay2,LOW);
+  digitalWrite(relay3,LOW);
+  digitalWrite(relay4,LOW);
+}
+//==========Setup==========//
+
+
+
+//=====Loop Function======// 
+void loop() {
+  digitalWrite(relay1,LOW);
+  delay(1000);
+  digitalWrite(relay1,HIGH);
+  delay(1000);
+
+  digitalWrite(relay2,LOW);
+  delay(1000);
+  digitalWrite(relay2,HIGH);
+  delay(1000);
+
+  digitalWrite(relay3,LOW);
+  delay(1000);
+  digitalWrite(relay3,HIGH);
+  delay(1000);
+
+  digitalWrite(relay4,LOW);
+  delay(1000);
+  digitalWrite(relay4,HIGH);
+  delay(1000);
+
+  digitalWrite(relay1,LOW);
+  digitalWrite(relay2,LOW);
+  digitalWrite(relay3,LOW);
+  digitalWrite(relay4,LOW);
+  delay(1000);
+
+  digitalWrite(relay1,HIGH);
+  digitalWrite(relay2,HIGH);
+  digitalWrite(relay3,HIGH);
+  digitalWrite(relay4,HIGH);
+  delay(1000);
+}
+//=====Loop Function======// 
