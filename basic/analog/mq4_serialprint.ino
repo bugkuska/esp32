641c465c29d1e1dc957288265aebe86a48e4700b
bugkuska <62003765+bugkuska@users.noreply.github.com>
2023-07-25 14:12:16 +0700

    Update mq4_serialprint.ino

--- basic/analog/mq4_serialprint.ino
+++ basic/analog/mq4_serialprint.ino
@@ -1,1 +1,16 @@
-coming soon
+//#define INPUT_1 35
+#define INPUT_2 34
+//#define INPUT_3 36
+//#define INPUT_4 39
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);     
+}
+void loop() {
+  int sensor_analog2;
+  sensor_analog2 = analogRead(INPUT_2);
+  Serial.print("Methane gas ADC : ");
+  Serial.println(sensor_analog2);
+  delay(1000);
+}
