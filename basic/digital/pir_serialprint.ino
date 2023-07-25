3bb88e019fa94951fbe8822a048ae84746c19b71
bugkuska <62003765+bugkuska@users.noreply.github.com>
2023-07-25 13:17:55 +0700

    Update pir_serialprint.ino

--- basic/digital/pir_serialprint.ino
+++ basic/digital/pir_serialprint.ino
@@ -1,1 +1,25 @@
-coming soon
+#define ledpin 2
+#define pirpin 19
+int val = 0;
+void setup() {
+  pinMode(ledpin, OUTPUT);
+  pinMode(pirpin, INPUT);
+
+  digitalWrite(ledpin, HIGH);
+  Serial.begin(9600);
+}
+
+void loop() {
+  val = digitalRead(pirpin);
+  Serial.print("val = ");
+  Serial.println(val);
+  if (val == 1) {
+    digitalWrite(ledpin, HIGH);
+    Serial.print("Motion detect");
+  }
+  else {
+    digitalWrite(ledpin, LOW);
+    Serial.print("No things");
+  }
+  delay(100);
+}
