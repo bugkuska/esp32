06b64a1611816d58d7df577422e3564a91cd9fb9
bugkuska <62003765+bugkuska@users.noreply.github.com>
2023-07-25 13:52:43 +0700

    Update soilmoisture.ino

--- basic/analog/soilmoisture.ino
+++ basic/analog/soilmoisture.ino
@@ -8,42 +8,15 @@ void setup() {
   Serial.begin(9600);     
 }
 void loop() {
- /*
-  float ldr_percentage1;
+  float moisture_percentage1;
   int sensor_analog1;
   sensor_analog1 = analogRead(INPUT_1);
-  Serial.print("Law LDR data 1:");
+  Serial.print("Law Soil data 1:");
   Serial.println(sensor_analog1);
-  ldr_percentage1 = ( 100 - ( (sensor_analog1/4095.00) * 100 ) );
-  
-  Serial.print("LDR Percentage 1 = ");
-  Serial.print(ldr_percentage1);
-  Serial.print("%\n\n");
-  delay(1000);  
-  */
+  moisture_percentage1 = (100 - ((sensor_analog1 / 4095.00) * 100));
 
-  float moisture_percentage2;
-  int sensor_analog2;
-  sensor_analog2 = analogRead(INPUT_2);
-  Serial.print("Law Soil data 2:");
-  Serial.println(sensor_analog2);
-  moisture_percentage2 = (100 - ((sensor_analog2 / 4095.00) * 100));
-
-  Serial.print("Moisture Percentage 2= ");
-  Serial.print(moisture_percentage2);
+  Serial.print("Moisture Percentage 1= ");
+  Serial.print(moisture_percentage1);
   Serial.print("%\n\n");
   delay(1000);
-/*
-  float moisture_percentage4;
-  int sensor_analog4;
-  sensor_analog4 = analogRead(INPUT_4);
-  Serial.print("Law Soil data 4:");
-  Serial.println(sensor_analog4);
-  moisture_percentage4 = ( 100 - ( (sensor_analog4/4095.00) * 100 ) );
-  
-  Serial.print("Moisture Percentage 4= ");
-  Serial.print(moisture_percentage4);
-  Serial.print("%\n\n");
-  delay(1000); 
-  */ 
 }
