10b75bb6eafdcb29f57a20d7102b400e18b5c09c
bugkuska <62003765+bugkuska@users.noreply.github.com>
2023-07-25 14:39:25 +0700

    Update lcddht11.ino

--- basic/lcd/lcddht11.ino
+++ basic/lcd/lcddht11.ino
@@ -1,6 +1,6 @@
 #include<Wire.h>
 #include<LiquidCrystal_I2C.h>      //https://github.com/bugkuska/esp32/raw/main/basic/lcd/LiquidCrystal_i2c.zip
-LiquidCrystal_I2C lcd(0x27,16,2);
+LiquidCrystal_I2C lcd(0x27,20,4);
 
 //dht11,dht22
 #include <DHT.h>                  //https://github.com/adafruit/DHT-sensor-library
